2459c05bafb10a3985258646031cb85e040c043e
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2025-02-20 21:07:57 +0100

    commit

---
+++ BlazorServerApp.SnakeGame/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using Snake.Game;
+
+namespace Blazor.Snake;
+
+
+
+
+public partial class MainWindowViewModel : ObservableObjects
+{
+	#region Fields
+
+	private Random _random = new Random();
+
+	private const int MAX_GAME_GRID_SIZE = 400;
+	private const int MAX_GAME_GRID_ROWS = 40;
+	private const int MAX_GAME_GRID_COLUMNS = 40;
+	private const int HIGH_SCORES_PATH = "Resources/HighScores.json";
+
+	private Direction _currentDirection = Direction.Down;
+	private Queue<NextMove> _nextMoves = new Queue<NextMove>();
+	private object _lock = new object();
+	private int _snakeSpeed = 200;
+
+	#endregion
+
+	#region Properties
+
+	public int GameGridSize => MAX_GAME_GRID_SIZE;
+
+
+	#endregion
+
+
+
+
+
+
+}
